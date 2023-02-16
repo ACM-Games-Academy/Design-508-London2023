3bdb3a4363be8a4ff037c6c694e0738e9aaec29c
hs1414 <hs1414@my.acm.ac.uk>
2023-02-16 02:05:25 +0000

    turret aiming at player

---
+++ Assets/Shooter.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public enum behaviours{search,aim,destroy};
+    [SerializeField] behaviours state;
+    [SerializeField] string targetTag;
+    Transform target;
+    Transform pointer;
+    [Header("Detecting The Target")]
+    [SerializeField] LayerMask WhatBlocksMyView;
+    [SerializeField] [Range(1,100)]float detectionDistance;
+    [Header("Aiming at the Target")]
+    [SerializeField] Transform Xrotater;
+    [SerializeField] float Xoffset;
+    [SerializeField] float Yoffset;
+    [SerializeField] Transform Yrotater;
+    [SerializeField] float rotationSpeed;
+    [Header("Shooting at the Target")]
+    [SerializeField] Transform Shootpoint;
+    [SerializeField] GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag(targetTag).transform;
+        pointer = Instantiate(new GameObject("pointer"), transform).transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Detection();
+        if(state == behaviours.search)
+        {
+            //look around animation
+        }
+        else if(state == behaviours.aim)
+        {
+            AimAtPlayer();
+        }
+    }
+
+
+    void Detection()
+    {
+        pointer.LookAt(target);
+        bool hit = Physics.Raycast(Shootpoint.position, pointer.forward, out RaycastHit ray, detectionDistance, WhatBlocksMyView);
+        if (hit)
+        {
+            if(ray.collider.gameObject == target.gameObject && state == behaviours.search)
+            {
+                state = behaviours.aim;
+            }
+            else
+            {
+                state = behaviours.search;
+            }
+        }
+        else
+        {
+            state = behaviours.search;
+        }
+        Debug.DrawRay(Shootpoint.position, pointer.forward*detectionDistance, Color.blue);
+    }
+
+    void AimAtPlayer()
+    {
+        float XAngle = 180 + Mathf.Atan2(Xrotater.position.y - target.position.y, Xrotater.position.z + Mathf.Abs(target.position.z)) * Mathf.Rad2Deg + Xoffset;//Find X angle toward target
+        Xrotater.localRotation = Quaternion.Euler(XAngle, 0, 0);//Quaternion.Euler(Mathf.LerpAngle(Xrotater.localRotation.x, XAngle, rotationSpeed * Time.deltaTime), 0, 0);//Lerp rotation to target X angle
+
+        float YAngle = 180 + Mathf.Atan2(Yrotater.position.x - target.position.x, Yrotater.position.z - target.position.z) * Mathf.Rad2Deg + Yoffset;//Find Y angle toward target
+        Yrotater.localRotation = Quaternion.Euler(0,YAngle, 0);//Quaternion.Euler(0,Mathf.LerpAngle(Yrotater.localRotation.x, YAngle, rotationSpeed * Time.deltaTime), 0);//Lerp rotation to target Y angle
+    }
+}
