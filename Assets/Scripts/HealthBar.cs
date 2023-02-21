431d212fd7418dab14839a1e06d1baffca8fc8fe
Noir-96 <115459699+Noir-96@users.noreply.github.com>
2023-02-21 19:02:18 +0000

    health bar implementation p2

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class HealthBar : MonoBehaviour
 {
     public float health;
+<<<<<<< Updated upstream
     public float maxHealth;
     // Start is called before the first frame update
     void Start()
     {
         maxHealth = PlayerController.playerHealth.maxHealth;
+=======
+    //public float maxHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
         health = PlayerController.playerHealth.health;
+<<<<<<< Updated upstream
         transform.localScale = new Vector3(health / maxHealth, 1, 1);
+=======
+>>>>>>> Stashed changes
     }
 }
