698aaca909a3e96228534170d81ed460a970ea4f
Noir-96 <115459699+Noir-96@users.noreply.github.com>
2023-02-21 19:01:23 +0000

    Health bar implementation

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = PlayerController.playerHealth.maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = PlayerController.playerHealth.health;
+        transform.localScale = new Vector3(health / maxHealth, 1, 1);
+    }
+}
