fade9d9320599201505061c950967165a5c46564
hs1414 <hs1414@my.acm.ac.uk>
2023-03-01 16:01:54 +0000

    created tutorial prompt prefab

---
+++ Assets/TutorialPrompt.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TutorialPrompt : MonoBehaviour
+{
+    [Header("Tags")]
+    [SerializeField] string setOffTag = "Player";
+    [SerializeField] string uiTextTag = "tutorialText";
+    [Header("Type tutorial message here:")]
+    [SerializeField] string tutorialMessage;
+    TextMeshProUGUI tutorialText;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tutorialText = GameObject.FindGameObjectWithTag("tutorialText").GetComponent<TextMeshProUGUI>();
+        tutorialText.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            tutorialText.gameObject.SetActive(true);
+            tutorialText.text = tutorialMessage;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            tutorialText.gameObject.SetActive(false);
+            tutorialText.text = null;
+        }
+    }
+}
