32b990ddac0c58c4e7c5581c4db3d626802904c9
hs1414 <hs1414@my.acm.ac.uk>
2023-03-13 23:28:22 +0000

    made thrown objects destroy on impact

--- Assets/Throwable.cs
+++ Assets/Throwable.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Throwable : MonoBehaviour
 {
     public Vector3 holdRotation;
+    public bool thrown;
+    [SerializeField] LayerMask destroyMask;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,5 +15,16 @@ public class Throwable : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(thrown && collision.gameObject != gameObject)
+        {
+            if(TryGetComponent(out HealthManager health))
+            {
+                health.Death();
+            }
+        }
     }
 }
