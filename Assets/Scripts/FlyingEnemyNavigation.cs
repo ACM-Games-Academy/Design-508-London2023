46eb7c61c4cb8b034175f9c8f030b4e5e52f1f50
hs1414 <hs1414@my.acm.ac.uk>
2023-03-15 22:42:04 +0000

    added working chopper prefab thing

--- Assets/Scripts/FlyingEnemyNavigation.cs
+++ Assets/Scripts/FlyingEnemyNavigation.cs
@@ -7,17 +7,22 @@ public class FlyingEnemyNavigation : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private Transform movepositionTransform;
+    [SerializeField] Transform helicopter;
+    float chopperHeight;
 
     private NavMeshAgent navMeshAgent;
 
     private void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        print(navMeshAgent.gameObject.name);
+        chopperHeight = helicopter.transform.position.y;
     }
 
     private void Update()
     {
         navMeshAgent.destination = movepositionTransform.position;
+        helicopter.position = new Vector3(helicopter.position.x, chopperHeight, helicopter.position.z);
     }
 
 
