d39fd61c98c385433ea94781fdd9a6d018ff4269
hs1414 <hs1414@my.acm.ac.uk>
2023-04-11 15:22:22 +0100

    slight tweaks to breakable script

--- Assets/Breakable.cs
+++ Assets/Breakable.cs
@@ -2,21 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(HealthManager))]
 public class Breakable : MonoBehaviour
 {
-    public GameObject Breaks;
+    public GameObject inactivePieces;
     public float breakForce;
 
-    void Update()
+    void Awake()
     {
-        
+        inactivePieces.SetActive(false);
     }
 
     public void BreakIt()
     {
-        GameObject Break = Instantiate(Breaks, transform.position, transform.rotation);
-
-        foreach(Rigidbody rb in Break.GetComponentsInChildren<Rigidbody>())
+        inactivePieces.SetActive(true);
+        inactivePieces.transform.SetParent(null);
+        foreach(Rigidbody rb in inactivePieces.GetComponentsInChildren<Rigidbody>())
         {
             Vector3 force = (rb.transform.position - transform.position) * breakForce;
         }
