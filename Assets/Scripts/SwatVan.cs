d2d5bc4c62fbbe5e5fadb2f9fe3bbb795a44db83
hs1414 <hs1414@my.acm.ac.uk>
2023-04-14 20:07:33 +0100

    swat van should now work

--- Assets/Scripts/SwatVan.cs
+++ Assets/Scripts/SwatVan.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SwatVan : Enemy
 {
@@ -9,18 +10,49 @@ public class SwatVan : Enemy
     public List<GameObject> troops;
     [SerializeField] Transform troopSpawnPoint;
     [SerializeField] float troopSpawnDelay;
+    bool deployed;
+    public override void Awake()
+    {
+        base.Awake();
+        deployed = false;
+        ToggleThrowable(false);
+    }
+
     public override bool PlayerInRange()
     {
-        if (tooClose())
+        if (tooClose() && !deployed)
         {
-
+            DeployTroops();
         }
         return base.PlayerInRange(); 
     }
 
-    void UnloadTroops()
+    IEnumerator DeployTroops()
     {
+        agent.enabled = false;
+        foreach(GameObject troop in troops)
+        {
+            yield return new WaitForSeconds(troopSpawnDelay);
+            if(troop.TryGetComponent(out Enemy enemyScript))
+            {
+                Instantiate(troop, troopSpawnPoint.position, troopSpawnPoint.rotation);
+            }
+        }
+        deployed = true;
+        ToggleThrowable(true);
+    }
 
+    void ToggleThrowable(bool toggle)
+    {
+        if(TryGetComponent(out Throwable ts))
+        {
+            ts.enabled = toggle;
+        }
+        if(TryGetComponent(out Rigidbody rb))
+        {
+            rb.isKinematic = !toggle;
+        }
+        agent.enabled = !toggle;
     }
 
 }
--- Assets/teleport.cs
+++ Assets/teleport.cs
@@ -6,20 +6,30 @@ public class teleport : MonoBehaviour
 {
     [SerializeField] Transform destination;
     [SerializeField] string teleportTag = "Player";
+    [SerializeField] float teleportDelay;
+    Transform transformToMove;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        transformToMove = GameObject.FindGameObjectWithTag(teleportTag).transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void Teleport(Transform teleportThis)
+    void Teleport()
     {
-       
+        transformToMove.position = destination.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == teleportTag)
+        {
+            Invoke("Teleport", 1);
+        }
     }
 }
