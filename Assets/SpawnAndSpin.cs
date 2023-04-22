6719bcd71e213717c7788eb5d1a0b933d1489ade
hs1414 <hs1414@my.acm.ac.uk>
2023-04-22 22:10:36 +0100

    changed that crosshair + main menu spin

---
+++ Assets/SpawnAndSpin.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAndSpin : MonoBehaviour
+{
+    [SerializeField] GameObject level;
+    public float t;
+    public Transform roticeryObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(level, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Lerp(transform.rotation, roticeryObject.rotation, t * Time.deltaTime);
+    }
+}
