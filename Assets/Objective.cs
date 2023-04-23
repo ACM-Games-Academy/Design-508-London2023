b1309fdbdca82a87fd4ac52529ebcd8d2fa94cbb
hs1414 <hs1414@my.acm.ac.uk>
2023-04-23 17:17:02 +0100

    fixed objective script

--- Assets/Objective.cs
+++ Assets/Objective.cs
@@ -24,7 +24,7 @@ public class Objective : MonoBehaviour
             ObjectiveManager.currentObjectiveText = objectiveMessage;
             ObjectiveManager.currentObjectiveLocation = objectiveLocation.position;
             ObjectiveManager.UpdateObjective();
-            print("sdujhbsdklfb");
+            Destroy(gameObject);
         }
     }
 }
